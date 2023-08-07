116b605ad95667c775e4aaa548819e69ad32e0f8
gkdlrjf87 <gkdlrjf87@naver.com>
2023-08-07 10:13:03 +0900

    20230807

--- src/ex21jdbc/connect/IConnectImpl.java
+++ src/ex21jdbc/connect/IConnectImpl.java
@@ -1,10 +1,12 @@
 package ex21jdbc.connect;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 /*
 인터페이스를 구현한 클래스로 extends 대신 implements를 사용한다
@@ -16,8 +18,8 @@ public class IConnectImpl implements IConnect {
 	public Connection con; //DB연결
 	public PreparedStatement psmt; //동적쿼리문 실행
 	public ResultSet rs; //select실행결과 반환
-	//public Statement stmt; //정적쿼리 실행
-	//public CallableStatement csmt; //프로시저 실행
+	public Statement stmt; //정적쿼리 실행
+	public CallableStatement csmt; //프로시저 실행
 	
 	//기본생성자
 	public IConnectImpl() {
@@ -75,8 +77,8 @@ public class IConnectImpl implements IConnect {
 			if(psmt!=null)psmt.close();
 			if(rs!=null)rs.close();
 			//필요한 경우 활성화해서 사용하면 된다.
-//			if(stmt!=null)stmt.close();
-//			if(csmt!=null)csmt.close();
+			if(stmt!=null)stmt.close();			
+			if(csmt!=null)csmt.close();
 			
 			System.out.println("자원 반납 완료");
 		} 
